0ca4f86fbd020ade733749a8ad4f22e5cf594709
Jayapal Reddy <saijoshna@Vasudeva.local>
2026-06-25 18:25:21 +0530

    Fix Vercel SPA routing by conditionally loading AdminApp based on domain and path

--- src/website-main.tsx
+++ src/website-main.tsx
@@ -2,15 +2,35 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import WebsiteApp from './WebsiteApp';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <WebsiteApp />
-      </BrowserRouter>
-    </HelmetProvider>
-  </StrictMode>
-);
+const root = createRoot(document.getElementById('root')!);
+
+const isAdminDomain = window.location.hostname.startsWith('admin');
+const isAdminPath = window.location.pathname.startsWith('/manage');
+
+if (isAdminDomain || isAdminPath) {
+  import('./AdminApp').then(({ default: AdminApp }) => {
+    root.render(
+      <StrictMode>
+        <HelmetProvider>
+          <BrowserRouter>
+            <AdminApp />
+          </BrowserRouter>
+        </HelmetProvider>
+      </StrictMode>
+    );
+  });
+} else {
+  import('./WebsiteApp').then(({ default: WebsiteApp }) => {
+    root.render(
+      <StrictMode>
+        <HelmetProvider>
+          <BrowserRouter>
+            <WebsiteApp />
+          </BrowserRouter>
+        </HelmetProvider>
+      </StrictMode>
+    );
+  });
+}
